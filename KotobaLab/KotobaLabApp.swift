d867b4f82a0fe33eea3b9ee0ec39819aafe2ecd9
sakura0224 <shiinayane@gmail.com>
2026-03-26 03:07:56 +0900

    Initial Commit

---
+++ KotobaLab/KotobaLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  KotobaLabApp.swift
+//  KotobaLab
+//
+//  Created by 椎名アヤネ on 2026/03/26.
+//
+
+import SwiftUI
+
+@main
+struct KotobaLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
