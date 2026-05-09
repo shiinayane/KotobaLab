ffe01806948cb9461c2f1474f9f56df29cb00418
shiinayane <shiinayane@gmail.com>
2026-05-09 16:00:53 +0900

    feat: add the Test system, implement a test for ToggleSavedWordUseCase

---
+++ KotobaLabTests/ToggleSavedWordUseCaseTests.swift
@@ -0,0 +1,55 @@
+//
+//  ToggleSavedWordUseCaseTests.swift
+//  KotobaLab
+//
+//  Created by 椎名アヤネ on 2026/05/09.
+//
+
+import Foundation
+import Testing
+@testable import KotobaLab
+
+struct ToggleSavedWordUseCaseTests {
+    
+    @Test
+    @MainActor
+    func toggleSavedWord_whenWordIsNotSaved_shouldSavedWord() throws {
+        // Arrange / Given
+        let repository = MockUserDataRepository(savedWordsRecord: [])
+        
+        let useCase = ToggleSavedWordUseCase(
+            wordID: 1,
+            userDataRepository: repository
+        )
+        
+        // Act / When
+        let isSaved = try useCase.execute()
+        
+        // Assert / Then
+        #expect(isSaved == true)
+        #expect(try repository.isWordSaved(wordID: 1) == true)
+    }
+    
+    @Test
+    @MainActor
+    func toggleSavedWord_whenWordIsSaved_shouldUnsavedWord() throws {
+        let repository = MockUserDataRepository(
+            savedWordsRecord: [
+                SavedWordRecordData(
+                    wordID: 1,
+                    savedAt: Date()
+                )
+            ]
+        )
+        
+        let useCase = ToggleSavedWordUseCase(
+            wordID: 1,
+            userDataRepository: repository
+        )
+        
+        let isSaved = try useCase.execute()
+        
+        #expect(isSaved == false)
+        #expect(try repository.isWordSaved(wordID: 1) == false)
+    }
+}
